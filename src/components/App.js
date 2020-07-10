85ed4f95a3482f426c8132902d0b8ac29d236615
amine <amine.hadef.dev@gmail.com>
2020-07-10 16:47:12 +0200

    firste commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../assets/css/App.css';
+
+import Header from './Header'
+import NavBottom from './NavBottom'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <NavBottom />
+    </div>
+  );
+}
+
+export default App;
