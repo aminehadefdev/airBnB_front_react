e7692eebc06aa4732557b72ab4d6d451dd129dc1
amine <amine.hadef.dev@gmail.com>
2020-07-15 09:41:14 +0200

    form register user

--- src/components/ModelSignUp.js
+++ src/components/ModelSignUp.js
@@ -1,14 +1,64 @@
 import React from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
-import navBottome from '../assets/css/ModelSignUp.css'
+import axios from 'axios';
+import '../assets/css/ModelSignUp.css'
+import queryString from 'query-string'
 
 class ModelSignUp extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+            city: '',
+            description: '',
+            birthday: '',
+        }
+        this.handleChange = this.handleChangeFirstName.bind(this);
+        this.handleChange = this.handleChangeLastName.bind(this);
+        this.handleChange = this.handleChangeEmail.bind(this);
+        this.handleChange = this.handleChangePassword.bind(this);
+        this.handleChange = this.handleChangeCity.bind(this);
+        this.handleChange = this.handleChangeDescription.bind(this);
+        this.handleChange = this.handleChangeBirthday.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-    handlSubmit = (event) => {
-        console.log('ok')
+    handleSubmit(event) {
+        event.preventDefault()
+        var { firstName, lastName, email, password, city, description, birthday } = this.state
+        var url = "http://localhost:8000/api/register/host/user"
+
+        axios.post(url, queryString.stringify({ firstName, lastName, email, password, city, description, birthday }), { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
+            .then(response => {
+                console.log(response)
+            }).catch(error => {
+                console.log(error.response)
+            });
     }
+    handleChangeFirstName = (event) => {
+        this.setState({ firstName: event.target.value })
+    }
+    handleChangeLastName = (event) => {
+        this.setState({ lastName: event.target.value })
+    }
+    handleChangeEmail = (event) => {
+        this.setState({ email: event.target.value })
+    }
+    handleChangePassword = (event) => {
+        this.setState({ password: event.target.value })
+    }
+    handleChangeCity = (event) => {
+        this.setState({ city: event.target.value })
+    }
+    handleChangeDescription = (event) => {
+        this.setState({ description: event.target.value })
+    }
+    handleChangeBirthday = (event) => {
+        this.setState({ birthday: event.target.value })
+    }
+
     render() {
         return (
             <Modal {...this.props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered >
@@ -18,40 +68,40 @@ class ModelSignUp extends React.Component {
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <Form onSubmit={this.handlSubmit}>
+                    <Form onSubmit={this.handleSubmit}>
                         <Form.Group controlId="formGroupText">
                             <Form.Label>firstName</Form.Label>
-                            <Form.Control type="text" placeholder="firstName" />
+                            <Form.Control name="firstName" type="text" placeholder="firstName" onChange={this.handleChangeFirstName} value={this.state.firstName} />
                         </Form.Group>
 
                         <Form.Group controlId="formGroupText">
                             <Form.Label>lastName</Form.Label>
-                            <Form.Control type="text" placeholder="lastName" />
+                            <Form.Control name="lastName" type="text" placeholder="lastName" onChange={this.handleChangeLastName} value={this.state.lastName} />
                         </Form.Group>
 
                         <Form.Group controlId="formGroupEmail">
                             <Form.Label>email</Form.Label>
-                            <Form.Control type="email" placeholder="email" />
+                            <Form.Control name="email" type="email" placeholder="email" onChange={this.handleChangeEmail} value={this.state.email} />
                         </Form.Group>
 
                         <Form.Group controlId="formGroupPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control name="password" type="password" placeholder="Password" onChange={this.handleChangePassword} value={this.state.password} />
                         </Form.Group>
 
                         <Form.Group controlId="formGroupText">
                             <Form.Label>city</Form.Label>
-                            <Form.Control type="text" placeholder="city" />
+                            <Form.Control name="city" type="text" placeholder="city" onChange={this.handleChangeCity} value={this.state.city} />
                         </Form.Group>
 
                         <Form.Group controlId="formGroupText">
                             <Form.Label>description</Form.Label>
-                            <Form.Control as="textarea" rows="3" placeholder="description" />
+                            <Form.Control name="description" as="textarea" rows="3" placeholder="description" onChange={this.handleChangeDescription} value={this.state.description} />
                         </Form.Group>
 
                         <Form.Group controlId="formGroupDate">
                             <Form.Label>birthday</Form.Label>
-                            <Form.Control type="date" placeholder="birthday" />
+                            <Form.Control name="birthday" type="date" placeholder="birthday" onChange={this.handleChangeBirthday} value={this.state.birthday} />
                         </Form.Group>
                         <Button type="submit">ok</Button>
                     </Form>
